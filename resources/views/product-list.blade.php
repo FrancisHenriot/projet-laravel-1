ab1528bcaff2454e77e6ac97eba7864588c6f89a
Francis Henriot <francis.henriot@le-campus-numerique.fr>
2021-02-03 17:08:19 +0100

    Tentative "trier par"

--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -3,6 +3,19 @@
 @section('title', 'Produits')
 
 @section('content')
+    <!--<div class="container mt-3">
+        <div class="input-group mb-3">
+            <label class="input-group-text" for="inputGroupSelect01">Trier par : </label>
+            <form action="{{ route('product-list') }}" method="GET">
+                <select name="sortBy" class="form-select" id="inputGroupSelect04">
+                    <option selected>Selectionner le type de tri...</option>
+                    <option value="name">Nom</option>
+                    <option value="price">Prix</option>
+                </select>
+                <button class="btn btn-outline-secondary" type="submit">Button</button>
+            </form>
+        </div>
+    </div>-->
     <div class="container mt-3">
         <div class="row row-cols-1 row-cols-md-2 g-4">
                 @foreach ($products as $product)
