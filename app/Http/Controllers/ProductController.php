afb2d984a5725fa0d3a15f801baa922003514933
Francis Henriot <francis.henriot@le-campus-numerique.fr>
2021-02-03 13:53:17 +0100

    Accès BDD - Page produit et détail produit

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,12 +9,22 @@ class ProductController extends Controller
 {
     public function showDetail($id)
     {
-        return view('product-detail', ['id' => $id]);
+        //$product = DB::select('select * from products where id = ?', [$id]);
+
+        $product = DB::table('products')
+            ->where('id', '=', $id)
+            ->get();
+
+        return view('product-detail', ['product' => $product[0]]);
     }
 
     public function showList()
     {
-        $products = DB::select('select * from products');
+        //$products = DB::select('select * from products');
+
+        $products = DB::table('products')
+            ->select('*')
+            ->get();
 
         return view('product-list', ['products' => $products]);
     }
--- resources/views/product-list.blade.php
+++ resources/views/product-list.blade.php
@@ -6,18 +6,20 @@
     <div class="container mt-3">
         <div class="row row-cols-1 row-cols-md-2 g-4">
                 @foreach ($products as $item)
-                    <div class="col">
-                        <div class="card">
-                            <img src="..." class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $item->name }}</h5>
-                                <p class="card-text">{{ $item->description }}</p>
-                            </div>
-                            <div class="col me-2 align-self-end">
-                                <h4>Prix : {{ $item->price }} €</h4>
+                    <a href="{{ route('product-detail', $item->id) }}">
+                        <div class="col">
+                            <div class="card">
+                                <img src="..." class="card-img-top" alt="...">
+                                <div class="card-body">
+                                    <h5 class="card-title">{{ $item->name }}</h5>
+                                    <p class="card-text">{{ $item->description }}</p>
+                                </div>
+                                <div class="col me-2 align-self-end">
+                                    <h4>Prix : {{ $item->price }} €</h4>
+                                </div>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 @endforeach
 
         </div>
